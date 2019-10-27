8d424016fed4c719f2b415453b5f20e83a272b5f
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2019-10-27 06:30:46 +0400

    chore(client): customize sandbox vue app setup
    - add pug loader
    - add sass loader
    
    - install Element ui framework

--- client/packages/sandbox-app/src/App.vue
+++ client/packages/sandbox-app/src/App.vue
@@ -1,12 +1,11 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div#app
+    hello-world(msg="Welcome")
+    el-button el-button
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'app',
