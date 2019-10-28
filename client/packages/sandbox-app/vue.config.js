b8e2cac7d2489fb53e01f1d9f5ae92f8257cd8d0
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2019-10-28 19:55:23 +0400

    chore(client): setup GitHub pages deployment

--- client/packages/sandbox-app/vue.config.js
+++ client/packages/sandbox-app/vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/idem/'
+    : '/',
   chainWebpack: (config) => {
     config.resolve.symlinks(false)
   }
