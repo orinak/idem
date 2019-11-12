9b302ef2da849e7043a3f1ac80ae2ba9b18102ae
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2019-11-12 14:24:02 +0400

    fix: plug database (PouchDB)

--- server/app/server.js
+++ server/app/server.js
@@ -1,8 +1,24 @@
+const PouchDB = require('pouchdb')
+
+const memoryAdapter = require('pouchdb-adapter-memory')
+
 const createApp = require('.')
+
+//
 
 const PORT = process.env.NODE_PORT || 8080
 
-const app = createApp()
+//
+
+PouchDB.plugin(memoryAdapter)
+
+const db = new PouchDB('records', { adapter: 'memory' })
+
+//
+
+const app = createApp({ db })
+
+//
 
 app.listen(PORT, err => {
   if (err) {
--- server/app/src/app.js
+++ server/app/src/app.js
@@ -3,11 +3,26 @@ const Koa = require('koa')
 const logger = require('koa-logger')
 const bodyparser = require('koa-bodyparser')
 
-function createApp () {
+/**
+ * Create a Koa app instance
+ *
+ * @param {Object} opts
+ * @param {PouchDB} db
+ *
+ * @returns {Koa}
+ */
+
+function createApp (opts) {
   const app = new Koa()
+
+  app.context.db = opts.db
 
   app.use(logger())
   app.use(bodyparser())
+
+  app.use(async ctx => {
+    ctx.body = await ctx.db.allDocs()
+  })
 
   return app
 }
