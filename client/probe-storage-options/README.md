bffe70f0b97eb00fad47091a78af52a518dd90bb
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2019-12-11 02:26:24 +0400

    feat(client): add probe for storage options support

---
+++ client/probe-storage-options/README.md
@@ -0,0 +1,11 @@
+# `client-probe-storage-options`
+
+> TODO: description
+
+## Usage
+
+```js
+const Probe = require('@pouk/idem-client-probe-storage-options')
+
+// TODO: DEMONSTRATE API
+```
---
+++ client/probe-storage-options/src/StorageOptions.js
@@ -0,0 +1,50 @@
+const Future = require('fluture')
+
+const Trait = require('@pouk/idem-type-trait')
+
+// helpers
+
+const checkCookie = () => {
+  const { navigator } = window
+  return navigator.cookieEnabled
+}
+
+const checkLocalStorage = () => {
+  try {
+    return !!window.localStorage
+  } catch (err) {
+    return true // SecurityError when referencing it means it exists
+  }
+}
+
+const checkSessionStorage = () => {
+  try {
+    return !!window.sessionStorage
+  } catch (err) {
+    return true // SecurityError when referencing it means it exists
+  }
+}
+
+const getter = () => {
+  return {
+    cookies: checkCookie(),
+    localStorage: checkLocalStorage(),
+    sessionStorage: checkSessionStorage()
+  }
+}
+
+/**
+ * Probe to get storage options availability
+ *
+ * @returns {Future<Error|Trait>}
+ */
+
+function StorageOptions () {
+  return Future
+    .attempt(getter)
+    .map(Trait.GenericTrait)
+}
+
+// expose probe
+
+module.exports = StorageOptions
